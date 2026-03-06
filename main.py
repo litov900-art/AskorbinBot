ecd1ed3f6f722b3df9f1d20ced5379f697a0ed17
Askorbun <litov900@gmail.com>
2026-03-06 12:49:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
+
 dp = Dispatcher()
 
+# Команда /start
 @dp.message(Command('start'))
-async def start_command(message: types.Message) -> None:
+async def start_command(message: types.Message):
     kb = [
-        [types.KeyboardButton(text="Обо мне")],
-
-        [
-            types.KeyboardButton(text="Что смотрел"),
-            types.KeyboardButton(text="Портфолио")
-        ],
-        [
-            types.KeyboardButton(text="О тебе")
-        ]
+        [types.KeyboardButton(text="Мой дневник")],
+        [types.KeyboardButton(text="Купить ботов")],
+        [types.KeyboardButton(text="Портфолио")],
     ]
     keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
     await message.answer('Привет! это мой бот-визитка', reply_markup=keyboard)
 
+# Мой дневник (добавлена inline-кнопка с ссылкой)
+@dp.message(F.text == 'Мой дневник')
+async def about_me_handler(message: types.Message):
+    text = 'Рад, что ты спросил держи мой тгк'
+    kb = [
+        [types.InlineKeyboardButton(text="Перейти в дневник", url="https://t.me/+O1AyPb6vyBgzMzZi")],
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
+    await message.answer(text, reply_markup=keyboard)
 
+# Купить ботов (inline-кнопка с ссылкой)
+@dp.message(F.text == 'Купить ботов')
+async def shop_handler(message: types.Message):
+    text = 'Тут ты можешь купить ботов'
+    kb = [
+        [types.InlineKeyboardButton(text="Перейти в магазин", url="https://t.me/Bot_ShopROBOT")],
+    ]
+    # Исправлено: использовалось неправильное название аргумента
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=kb)
+    await message.answer(text, reply_markup=keyboard)
 
-@dp.message(F.text == 'Обо мне')
-async def about_me_handler(message: types.Message) -> None:
-    await message.answer('Рад, что ты спросил держи мой тгк\nhttps://t.me/+O1AyPb6vyBgzMzZi')
-
-@dp.message(F.text == 'Что смотрел')
-async def about_me_handler(message: types.Message) -> None:
-    await message.answer('Рад, что ты спросил держи мой профиль на кинопоиск\nhttps://www.kinopoisk.ru/user/158986273/')
-
-
+# Портфолио
 @dp.message(F.text == 'Портфолио')
 async def portfolio_handler(message: types.Message) -> None:
-    await message.answer('Работаю с Python Aiogram3 SQLite\nБоты в работе:\n@FeniX1ROBOT\nГотовые боты:\n@AskorbinROBot\n@stepashka1564Bot')
+    await message.answer('Работаю с Python Aiogram3 SQLite\n'
+                         'Боты в работе:\n@FeniX1ROBOT\n@Bot_ShopROBOT\n'
+                         'Готовые боты:\n@AskorbinROBot\n@stepashka1564Bot')
 
-
-@dp.message(F.text == 'О тебе')
-async def about_you_handler(message: types.Message) -> None:
-    await message.answer(f'А тебя зовут {message.from_user.full_name}!')
-
-
-async def main() -> None:
-    token = "8792022664:AAE-U-kISO_5cmiP34ZfwFxXNlWUmyVLYpE"
-    bot = Bot(token)
+# Запуск бота
+async def main():
+    bot = Bot(token="8792022664:AAE-U-kISO_5cmiP34ZfwFxXNlWUmyVLYpE")
     await dp.start_polling(bot)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
